1e91a27306102b4b96c099db7e90376879ee4f99
WebTechSmartSolution <webtechssolution@gmail.com>
2024-11-27 19:49:29 +0500

    Update nv.jsx

--- src/ownerProfile/components/nv.jsx
+++ src/ownerProfile/components/nv.jsx
@@ -16,25 +16,32 @@ const NavbarSidebar = () => {
   const [showSearchModal, setShowSearchModal] = useState(false);
   const [showUserModal, setShowUserModal] = useState(false);
   const { username } = useUser();
-
+  
   const searchRef = useRef(null);
   const userRef = useRef(null);
-
+  
   const toggleSidebar = () => {
-    setSidebarActive(!sidebarActive);
+    setSidebarActive((prevState) => {
+      if (prevState) setOpenDropdown(null);
+      return !prevState;
+    });
   };
-
+  
   const handleDropdownToggle = (dropdownName) => {
-    setOpenDropdown(openDropdown === dropdownName ? null : dropdownName);
+    if (sidebarActive) {
+      setOpenDropdown((prevDropdown) =>
+        prevDropdown === dropdownName ? null : dropdownName
+      );
+    }
   };
-
+  
   const handleLogout = () => {
     authService.logout();
-    
   };
+  
 
   return (
-    <div>
+    <div className="main3">
       
       {/* Navbar */}
 <nav className="custom-navbar1 fixed-top">
@@ -66,33 +73,24 @@ const NavbarSidebar = () => {
 
 
       {/* Sidebar */}
-      <div id="sidebar1" className={sidebarActive ? 'active1' : ''} onMouseEnter={() => setSidebarActive(true)} onMouseLeave={()=>setSidebarActive(false)}>
+      <div id="sidebar1" className={sidebarActive ? 'active1' : ''} >
         <ul className="p-3">
           <li className="active1">
-            <Link to="/portfolio/dashboard"><FontAwesomeIcon icon={faHome} /><span className="sidebar-text1"> Dashboard</span></Link>
+            <Link to="/portfolio/dashboard" onClick={() => setSidebarActive(false)}><FontAwesomeIcon icon={faHome} /><span className="sidebar-text1">Dashboard</span></Link>
           </li>
           <li>
-            <Link to="/portfolio/post-listing"><FontAwesomeIcon icon={faPlus} /><span className="sidebar-text1"> Post Listing</span></Link>
+            <Link to="/portfolio/post-listing" onClick={() => setSidebarActive(false)}><FontAwesomeIcon icon={faPlus} /><span className="sidebar-text1">Post Listing</span></Link>
           </li>
 
-          {/* Property Management Dropdown */}
+         <li>
+          <Link to="/portfolio/all-listing" onClick={() => setSidebarActive(false)}><FontAwesomeIcon icon={faTasks} /><span className="sidebar-text1">All Listings</span></Link>
+          </li>
           <li>
-            <button className="dropdown-toggle1" onClick={() => handleDropdownToggle('propertyManagement')}>
-              <FontAwesomeIcon icon={faTasks} /><span className="sidebar-text1"> Property Management</span>
-            </button>
-            {openDropdown === 'propertyManagement' && (
-              <ul className="collapse1 show">
-                <li><Link to="/portfolio/all-listing">All Listings</Link></li>
-              </ul>
-            )}
+            <Link to="/portfolio/inbox" onClick={() => setSidebarActive(false)}><FontAwesomeIcon icon={faEnvelope} /><span className="sidebar-text1">Inbox</span></Link>
           </li>
 
           <li>
-            <Link to="/portfolio/inbox"><FontAwesomeIcon icon={faEnvelope} /><span className="sidebar-text1"> Inbox</span></Link>
-          </li>
-
-          <li>
-            <Link to="/portfolio/profile-setting"><FontAwesomeIcon icon={faUserCog} /><span className="sidebar-text1"> User Settings</span></Link>
+            <Link to="/portfolio/profile-setting"  onClick={() => setSidebarActive(false)}><FontAwesomeIcon icon={faUserCog} /><span className="sidebar-text1">User Settings</span></Link>
           </li>
 
           <li>
@@ -101,8 +99,8 @@ const NavbarSidebar = () => {
             </button>
             {openDropdown === 'propShop' && (
               <ul className="collapse1 show">
-                <li><Link to="/portfolio/purchase-products">Buy Product</Link></li>
-                <li><Link to="/portfolio/order-history">Order History</Link></li>
+                <li><Link to="/portfolio/purchase-products"  >Buy Product</Link></li>
+                <li><Link to="/portfolio/order-history" >Order History</Link></li>
               </ul>
             )}
           </li>
@@ -138,7 +136,7 @@ const NavbarSidebar = () => {
       {showUserModal && (
         <div className="modal-overlay" onClick={() => setShowUserModal(false)}>
           <div className="modal-content1"  onClick={(e) => e.stopPropagation()}>
-            <h5>User Profile</h5>
+            <h5>Profile Setting</h5>
             <Link className="btn btn-outline-primary w-100 my-2" to="/portfolio/profile-setting">Account Settings</Link>
             <button className="btn btn-outline-danger w-100 my-2" onClick={handleLogout}>Logout</button>
             {/* <button className="btn btn-secondary w-100 my-2" onClick={() => setShowUserModal(false)}>Close</button> */}
